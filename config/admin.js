a80aa21ca10dfd920635ba399f38a698a1d54ec1
dong.pham <cpud1258@gmail.com>
2023-02-03 11:14:58 +0700

    add API_TOKEN_SALT

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,8 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  serveAdminPanel: env.bool('SERVE_ADMIN_PANEL', true), // http://yourbackend.com will not serve any static admin files
 });
