1eefef960bd55a8017e7f1ec1f8f56c468e9c703
dong.pham <cpud1258@gmail.com>
2023-02-03 10:56:22 +0700

    feat: update versions

---
+++ config/plugins.js
@@ -0,0 +1,9 @@
+// path: ./config/plugins.js
+
+module.exports = ({ env }) => ({
+  // enable a plugin that doesn't require any configuration
+  i18n: true,
+  seo: true,
+  documentation: true,
+  
+});
