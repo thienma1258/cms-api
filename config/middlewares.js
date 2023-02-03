6fa3d98ed70ba235bb4293bf93d3c3ea6b306cf2
dong.pham <cpud1258@gmail.com>
2023-02-03 14:20:56 +0700

    feat: allow cors and strapi security

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,40 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+            `https://${(env('DOMAIN'))}`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+            `https://${(env('DOMAIN'))}`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      header: '*',
+      origin: env('ALLOW_ORIGINS', '*'),
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
